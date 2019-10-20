9b5fd48137ffb986488df83b4433fc3cb1193a55
Tom Doherty <tom@singlesecond.com>
2019-10-20 02:21:05 +0100

    Sync

--- virt-install.sh
+++ virt-install.sh
@@ -1,6 +1,7 @@
 virt-install \
   --name k3s-master01 \
   --ram 4096 \
+  --disk size=10 \
   --quiet \
   --noautoconsole \
   --autostart \
@@ -12,11 +13,12 @@ virt-install \
   --graphics none \
   --initrd-inject ./ks/k3s-master01.ks \
   --location 'http://mirror.mhd.uk.as44574.net/mirror.centos.org/7.7.1908/os/x86_64/' \
-  --extra-args 'console=ttyS0,115200n8 ks=file:/k3s-master01.ks ksdevice=eth0 ip=192.168.0.100 netmask=255.255.255.0 nameserver=8.8.8.8 gateway=192.168.0.1' &
+  --extra-args 'console=ttyS0,115200n8 ks=file:/k3s-master01.ks ksdevice=eth0 ip=192.168.0.20 netmask=255.255.255.0 nameserver=8.8.8.8 gateway=192.168.0.1' &
 
 virt-install \
   --name k3s-node01 \
   --ram 4096 \
+  --disk size=10 \
   --quiet \
   --noautoconsole \
   --autostart \
@@ -28,12 +30,13 @@ virt-install \
   --graphics none \
   --initrd-inject ./ks/k3s-node01.ks \
   --location 'http://mirror.mhd.uk.as44574.net/mirror.centos.org/7.7.1908/os/x86_64/' \
-  --extra-args 'console=ttyS0,115200n8 ks=file:/k3s-node01.ks ksdevice=eth0 ip=192.168.0.101 netmask=255.255.255.0 nameserver=8.8.8.8 gateway=192.168.0.1' &
+  --extra-args 'console=ttyS0,115200n8 ks=file:/k3s-node01.ks ksdevice=eth0 ip=192.168.0.21 netmask=255.255.255.0 nameserver=8.8.8.8 gateway=192.168.0.1' &
 
 
 virt-install \
   --name k3s-node02 \
   --ram 4096 \
+  --disk size=10 \
   --quiet \
   --noautoconsole \
   --autostart \
@@ -45,11 +48,12 @@ virt-install \
   --graphics none \
   --initrd-inject ./ks/k3s-node02.ks \
   --location 'http://mirror.mhd.uk.as44574.net/mirror.centos.org/7.7.1908/os/x86_64/' \
-  --extra-args 'console=ttyS0,115200n8 ks=file:/k3s-node02.ks ksdevice=eth0 ip=192.168.0.102 netmask=255.255.255.0 nameserver=8.8.8.8 gateway=192.168.0.1' &
+  --extra-args 'console=ttyS0,115200n8 ks=file:/k3s-node02.ks ksdevice=eth0 ip=192.168.0.22 netmask=255.255.255.0 nameserver=8.8.8.8 gateway=192.168.0.1' &
 
 virt-install \
   --name k3s-node03 \
   --ram 4096 \
+  --disk size=10 \
   --quiet \
   --noautoconsole \
   --autostart \
@@ -61,4 +65,4 @@ virt-install \
   --graphics none \
   --initrd-inject ./ks/k3s-node03.ks \
   --location 'http://mirror.mhd.uk.as44574.net/mirror.centos.org/7.7.1908/os/x86_64/' \
-  --extra-args 'console=ttyS0,115200n8 ks=file:/k3s-node03.ks ksdevice=eth0 ip=192.168.0.103 netmask=255.255.255.0 nameserver=8.8.8.8 gateway=192.168.0.1' &
+  --extra-args 'console=ttyS0,115200n8 ks=file:/k3s-node03.ks ksdevice=eth0 ip=192.168.0.23 netmask=255.255.255.0 nameserver=8.8.8.8 gateway=192.168.0.1' &
