54e3a7ae90514eeaede46258ae7c801b2f7526eb
Tom Doherty <tom@singlesecond.com>
2020-04-04 03:39:23 +0100

    Switch to three more powerful nodes, update helm

--- virt-install.sh
+++ virt-install.sh
@@ -4,7 +4,7 @@ virsh list --all --name | grep k3s- |xargs -l -i echo 'virsh destroy {}; virsh u
 
 virt-install \
   --name k3s-master01 \
-  --ram 2048 \
+  --ram 4096 \
   --disk size=10 \
   --quiet \
   --noautoconsole \
@@ -22,14 +22,14 @@ virt-install \
 
 virt-install \
   --name k3s-node01 \
-  --ram 2048 \
+  --ram 4096 \
   --disk size=10 \
   --quiet \
   --noautoconsole \
   --autostart \
   --virt-type kvm \
   --wait=-1 \
-  --vcpus 1 \
+  --vcpus 2 \
   --os-type linux \
   --os-variant rhel7.7 \
   --network bridge=bridge0 \
@@ -40,14 +40,14 @@ virt-install \
 
 virt-install \
   --name k3s-node02 \
-  --ram 2048 \
+  --ram 4096 \
   --disk size=10 \
   --quiet \
   --noautoconsole \
   --autostart \
   --virt-type kvm \
   --wait=-1 \
-  --vcpus 1 \
+  --vcpus 2 \
   --os-type linux \
   --os-variant rhel7.7 \
   --network bridge=bridge0 \
@@ -56,23 +56,23 @@ virt-install \
   --location 'http://mirror.mhd.uk.as44574.net/mirror.centos.org/7.7.1908/os/x86_64/' \
   --extra-args 'console=ttyS0,115200n8 ks=file:/k3s-node02.ks ksdevice=eth0 ip=192.168.0.22 netmask=255.255.255.0 nameserver=8.8.8.8 gateway=192.168.0.1' &
 
-virt-install \
-  --name k3s-node03 \
-  --ram 2048 \
-  --disk size=10 \
-  --quiet \
-  --noautoconsole \
-  --autostart \
-  --virt-type kvm \
-  --wait=-1 \
-  --vcpus 1 \
-  --os-type linux \
-  --os-variant rhel7.7 \
-  --network bridge=bridge0 \
-  --graphics none \
-  --initrd-inject ./ks/k3s-node03.ks \
-  --location 'http://mirror.mhd.uk.as44574.net/mirror.centos.org/7.7.1908/os/x86_64/' \
-  --extra-args 'console=ttyS0,115200n8 ks=file:/k3s-node03.ks ksdevice=eth0 ip=192.168.0.23 netmask=255.255.255.0 nameserver=8.8.8.8 gateway=192.168.0.1' &
+# virt-install \
+#   --name k3s-node03 \
+#   --ram 2048 \
+#   --disk size=10 \
+#   --quiet \
+#   --noautoconsole \
+#   --autostart \
+#   --virt-type kvm \
+#   --wait=-1 \
+#   --vcpus 1 \
+#   --os-type linux \
+#   --os-variant rhel7.7 \
+#   --network bridge=bridge0 \
+#   --graphics none \
+#   --initrd-inject ./ks/k3s-node03.ks \
+#   --location 'http://mirror.mhd.uk.as44574.net/mirror.centos.org/7.7.1908/os/x86_64/' \
+#   --extra-args 'console=ttyS0,115200n8 ks=file:/k3s-node03.ks ksdevice=eth0 ip=192.168.0.23 netmask=255.255.255.0 nameserver=8.8.8.8 gateway=192.168.0.1' &
 
 
 while [[ $(virsh list --name | grep -c k3s) -lt $(($(grep -c -- '--name k3s' virt-install.sh) -1)) ]]; do
